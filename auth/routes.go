3a1ee54d0f1e00101142cf125980e098f57b194d
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2024-08-18 15:40:30 +0200

    feat: Add signout functionality

--- auth/routes.go
+++ auth/routes.go
@@ -10,6 +10,16 @@ func AuthRoutes(app *fiber.App) {
 	authGroup.Post("/signin", signin)
 	authGroup.Get("/signup", signupPage)
 	authGroup.Post("/signup", signup)
+	authGroup.Post("/signout", signout)
 	// authGroup.Get("/google", googleLogin)
 	// authGroup.Get("/google/callback", googleCallback)
+}
+
+func signout(c *fiber.Ctx) error {
+	c.Cookie(&fiber.Cookie{
+		Name:  "auth_token",
+		Value: "",
+	})
+	c.Set("HX-Redirect", "/")
+	return c.SendStatus(fiber.StatusOK)
 }
