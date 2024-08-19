32d2bf3ddea29252365f7670b30c1dec5dffdfaf
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2024-08-19 18:28:59 +0200

    feat: protect dashboard agains non existent document

--- dashboard/page.go
+++ dashboard/page.go
@@ -26,11 +26,23 @@ func dashboardPage(c *fiber.Ctx) error {
 
 	if rootId == "" {
 		rootId = "root"
-	}
+	} else {
+		if err != nil {
+			c.Set("HX-Redirect", "/auth/signin")
+			return c.Status(fiber.StatusInternalServerError).Redirect("/auth/signin")
+		}
 
-	if err != nil {
-		c.Set("HX-Redirect", "/auth/signin")
-		return c.Status(fiber.StatusInternalServerError).Redirect("/auth/signin")
+		// Check if the document exists and if it's a directory
+		doc, err := database.GetDocumentFromId(rootId)
+		if err != nil {
+			c.Set("HX-Redirect", "/dashboard/root")
+			return c.Status(fiber.StatusNotFound).Redirect("/dashboard")
+		}
+
+		if doc.Type != "directory" {
+			c.Set("HX-Redirect", "/dashboard/root")
+			return c.Status(fiber.StatusNotFound).Redirect("/dashboard")
+		}
 	}
 
 	page := dashboardView(user, rootId)
